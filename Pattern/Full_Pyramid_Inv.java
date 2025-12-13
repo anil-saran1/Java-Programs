b0c87d939b6d12327360bd94ca9f3ad7b8b64bc5
Anil Saran <saranil1112000@gmail.com>
2025-12-13 10:00:10 +0530

    Full_Pyramid_Inverted

---
+++ Pattern/Full_Pyramid_Inv.java
@@ -0,0 +1,26 @@
+class Full_Pyramid_Inv{
+
+    public static void main(String args[]){
+        /*
+        *********
+         *******
+          *****
+           ***
+            * 
+        */
+        //Outer loop
+        int n=5;
+        for(int i=0;i<n;i++){
+            //Space-
+            for(int j=0;j<i;j++){
+                System.out.print(" "); 
+            }
+            //Star-
+            for(int j=0;j<2*n-2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
