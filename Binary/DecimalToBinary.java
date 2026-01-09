ba921336823dffcd3991e76b0f818344766863b4
Anil Saran <saranil1112000@gmail.com>
2026-01-09 22:24:10 +0530

    DecimalTOBinary

---
+++ Binary/DecimalToBinary.java
@@ -0,0 +1,21 @@
+public class DecimalToBinary{
+
+    public static void D2B(int decNum){
+        int pow=0;
+        int binNum=0;
+
+        while(decNum > 0){
+            int rem= decNum % 2;
+            binNum=binNum + (rem * (int)Math.pow(10,pow));
+
+            pow++;
+            decNum=decNum/2;
+
+        }
+        System.out.println(binNum);
+    }
+
+    public static void main(String[] args) {
+        D2B(7);
+    }
+}
