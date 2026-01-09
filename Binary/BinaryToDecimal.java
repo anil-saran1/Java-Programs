81f48f041cc88ed875ca095405f97651da23608d
Anil Saran <saranil1112000@gmail.com>
2026-01-09 22:22:47 +0530

    BinaryToDecimal

---
+++ Binary/BinaryToDecimal.java
@@ -0,0 +1,20 @@
+public class BinaryToDecimal{
+
+    public static void B2D(int binNum){
+       int pow=0;
+       int decNum=0;
+
+       while(binNum >0){
+        int lastDigit= binNum % 10;
+        decNum = decNum +(lastDigit * (int)Math.pow(2,pow));
+        pow++;
+        binNum=binNum/10;
+       }
+       System.out.println(decNum);
+
+    }
+    public static void main(String args[]){
+        B2D(101);
+
+    }
+}
